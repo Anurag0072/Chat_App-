d0c9114a335c82b33da36aca4cd889a2ff98889a
Anurag Deshmukh <48847887+Anurag0072@users.noreply.github.com>
2022-10-05 18:42:13 +0530

    Add files via upload

---
+++ ChatApp/src/main/java/com/chat/app/controller/MessageController.java
@@ -0,0 +1,30 @@
+package com.chat.app.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chat.app.models.Message;
+
+@RestController
+public class MessageController {
+	
+	
+	@MessageMapping("/message")
+	@SendTo("/topic/return-to")
+	public Message getContent(@RequestBody Message message) {
+		
+		try {
+			
+			Thread.sleep(2);
+			
+		}catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		return message;
+	}
+	
+
+}
